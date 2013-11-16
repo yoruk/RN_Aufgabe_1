d92d7903db6a90b7daf4ab041ded4ffb38a3766c
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2013-11-16 05:56:41 +0100

    wip #3

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-gcc -Wall -g -pthread -c src/main.c -o src/main.o
-gcc -Wall -g -pthread -c src/buffer.c -o src/buffer.o
-gcc -Wall -g -pthread -c src/client.c -o src/client.o
-gcc -Wall -g -pthread -c src/server_handler.c -o src/server_handler.o
-gcc -Wall -g -pthread -c src/server.c -o src/server.o
-gcc -lpthread -lm -lopencv_core -lopencv_highgui -lopencv_imgproc -o aufgabe_1 src/main.o src/buffer.o src/client.o src/server_handler.o src/server.o
+gcc -Wall -g -c src/main.c -o src/main.o -pthread
+gcc -Wall -g -c src/buffer.c -o src/buffer.o -pthread
+gcc -Wall -g -c src/client.c -o src/client.o -pthread
+gcc -Wall -g -c src/server_handler.c -o src/server_handler.o -pthread
+gcc -Wall -g -c src/server.c -o src/server.o -pthread
+gcc -o aufgabe_1 src/main.o src/buffer.o src/client.o src/server_handler.o src/server.o -lpthread -lm -lopencv_core -lopencv_highgui -lopencv_imgproc
 
